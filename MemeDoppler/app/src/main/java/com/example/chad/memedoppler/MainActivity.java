2362d38303cd6417e7ddb18a66ff976064d2336b
Donoghue-Dennis <Donoghue.Dennis@gmail.com>
2016-11-13 18:42:12 -0500

    All the edits necessary for image zooming on main meme.

--- MemeDoppler/app/src/main/java/com/example/chad/memedoppler/MainActivity.java
+++ MemeDoppler/app/src/main/java/com/example/chad/memedoppler/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.chad.memedoppler;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
 
-    /** Called when the user clicks the Send button */
-    /* public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+
+    /* Called when the user clicks the Send button */
+    public void largeImage(View view) {
+        Intent intent = new Intent(this, ExpandedMeme.class);
         startActivity(intent);
-    } */
+    }
 }
