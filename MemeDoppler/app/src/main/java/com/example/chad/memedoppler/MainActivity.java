b8c4e48277c9eeb54411c1848107fd233a408004
Chad Klinefelter <klinefelter.chad@gmail.com>
2016-11-13 17:53:15 -0500

    Updated main menu formatting.

--- MemeDoppler/app/src/main/java/com/example/chad/memedoppler/MainActivity.java
+++ MemeDoppler/app/src/main/java/com/example/chad/memedoppler/MainActivity.java
@@ -5,13 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    // public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "Welcome back!", Toast.LENGTH_SHORT).show();
+        Calendar c = Calendar.getInstance();
+        int date = c.get(Calendar.DATE);
+        // final TextView currentWeatherText = (TextView)findViewById(R.id.current_weather_text);
+        // currentWeatherText.setText(date);
     }
 
     /** Called when the user clicks the Send button */
