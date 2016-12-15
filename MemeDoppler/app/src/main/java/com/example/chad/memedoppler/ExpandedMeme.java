63b104363bbb91159bf8c2f198f7c8ada17ac227
Donoghue-Dennis <Donoghue.Dennis@gmail.com>
2016-12-14 19:07:05 -0500

    added new pane files and moved old ones. Made a few changes to MainAct and Expanded Meme

--- MemeDoppler/app/src/main/java/com/example/chad/memedoppler/ExpandedMeme.java
+++ MemeDoppler/app/src/main/java/com/example/chad/memedoppler/ExpandedMeme.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class ExpandedMeme extends AppCompatActivity {
 
@@ -11,6 +14,10 @@ public class ExpandedMeme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expanded_meme);
+        int imageId = getIntent().getIntExtra("imageId",0);
+        Toast.makeText(getApplicationContext(), imageId, Toast.LENGTH_SHORT).show();
+        MyButton myButton = (MyButton) findViewById(R.id.main_Meme);
+        Picasso.with(getApplicationContext()).load(imageId).into(myButton);
     }
     public void MainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
