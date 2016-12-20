840f82f677d84514d7d07913af9e7071dd303986
Chad Klinefelter <klinefelter.chad@gmail.com>
2016-12-20 12:19:38 -0500

    Test.php

--- PHP/Test.php
+++ PHP/Test.php
@@ -1,37 +1,13 @@
 <?php
-require 'androidServer.php';
+use 'androidServer.php';
  
-/*class CalculatorTests extends PHPUnit_Framework_TestCase
+class LocationTests extends PHPUnit_Framework_TestCase
 {
-        private $calculator;
- 
-        protected function setUp()
+        public function zip1()
         {
-            $this->calculator = new Calculator();
+            $zip = "04101";
+            $this->assertEquals("04101", $zip);
         }
- 
-        protected function tearDown()
-        {
-            $this->calculator = NULL;
-        }
- 
-        public function testAdd()
-        {
-            $result = $this->calculator->add(1, 2);
-            $this->assertEquals(3, $result);
-        }
- 
-        public function testAddWithZero()
-        {
-            $result = $this->calculator->add(0, 0);
-            $this->assertEquals(0, $result);
-        }
- 
-        public function testAddWithNegative()
-        {
-            $result = $this->calculator->add(-1, -1);
-            $this->assertEquals(-2, $result);
-        }
- 
-}*/
+
+}
 
